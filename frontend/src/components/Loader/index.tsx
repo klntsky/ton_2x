0d4cd2de224bd4a9a875cb4639df9c794fb22ddb
Vishtar <viktor.kishchuk@gmail.com>
2024-07-04 13:05:06 +0400

    front: logo in the Loader

--- frontend/src/components/Loader/index.tsx
+++ frontend/src/components/Loader/index.tsx
@@ -1,5 +1,11 @@
 import s from './styles.module.css'
 
+import logo from './logo.png'
+
 export const Loader = () => {
-  return <div className={s.loader}></div>
+  return (
+    <div className={s.loader}>
+      <img src={logo} alt="Logo" className={s.logo} />
+    </div>
+  )
 }
